bf4843b7fded63eaf1b470b5b4ea9fd0ce50b019
bigcat2014 <ltogan.2014@gmail.com>
2018-04-24 01:45:08 -0400

    Added shape exercise

---
+++ Assignment 2/driver.py
@@ -0,0 +1,20 @@
+from shape import get_shape
+
+class Driver:
+	shapes = []
+	
+def main():
+	driver = Driver()
+
+	fileLines = []
+	with open('shapes.txt', 'r') as f:
+		fileLines = f.readlines()
+	
+	for line in fileLines:
+		driver.shapes.append(get_shape(line))
+
+	for shape in driver.shapes:
+		print('Shape: %s;\tsides: %s;\t\tarea: %.2f' % (shape.name, str(shape.sides), shape.area()))
+		
+if __name__ == '__main__':
+	main()
---
+++ Assignment 2/shape.py
@@ -0,0 +1,40 @@
+from math import sqrt
+
+class Shape:
+	sides = []
+	name = ''
+
+	def __init__(self):
+		self.name = 'Shape'
+	
+	def area(self):
+		pass
+
+
+class Rectangle(Shape):
+	def __init__(self, x, y):
+		self.name = 'Rectangle'
+		self.sides = [x, y]
+	
+	def area(self):
+		return self.sides[0] * self.sides[1]
+
+
+class Triangle(Shape):
+
+	def __init__(self, s1, s2, s3):
+		self.name = 'Triangle'
+		self.sides = [s1, s2, s3]
+	
+	def area(self):
+		p = (self.sides[0] + self.sides[1] + self.sides[2]) / 2
+		return sqrt(p * (p - self.sides[0]) * (p - self.sides[1]) * (p - self.sides[2]))
+
+
+def get_shape(line):
+	line = line.split()
+	shape_type = line[0]
+	if shape_type == 'T':
+		return Triangle(float(line[1]), float(line[2]), float(line[3]))
+	else:
+		return Rectangle(float(line[1]), float(line[2]))

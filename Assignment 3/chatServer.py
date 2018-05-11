fbc062fc1ca28f4ec8cc0dc7362eb3fb1a5168bf
bigcat2014 <ltogan.2014@gmail.com>
2018-05-11 13:34:04 -0400

    Bug fixes

--- Assignment 3/chatServer.py
+++ Assignment 3/chatServer.py
@@ -32,9 +32,9 @@ def chat_server():
             if sock == server_socket: 
                 sockfd, addr = server_socket.accept()
                 SOCKET_LIST.append(sockfd)
-                print("Client (%s) connected" % addr)
+                print("Client (%s,%s) connected" % addr)
                  
-                broadcast(server_socket, sockfd, "[%s] entered our chatting room\n" % addr)
+                broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
              
             # a message from a client, not a new connection
             else:
@@ -51,7 +51,7 @@ def chat_server():
                             SOCKET_LIST.remove(sock)
 
                         # at this stage, no data means probably the connection has been broken
-                        broadcast(server_socket, sock, "Client (%s) is offline\n" % addr)
+                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
 
                 # exception 
                 except:

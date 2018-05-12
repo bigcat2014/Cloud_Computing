386d808422e09938488dc803eded3eb55ed9fcff
bigcat2014 <ltogan.2014@gmail.com>
2018-05-11 20:24:39 -0400

    Added client

--- Assignment 3/ticTacToeServer.py
+++ Assignment 3/ticTacToeServer.py
@@ -5,8 +5,24 @@ import select
 
 HOST = ''
 SOCKET_LIST = []
+X_LIST = []
+O_LIST = []
 RECV_BUFFER = 4096
 PORT = 9009
+BOARD_SIZE = 9
+
+
+def get_game_board(board):
+	return_string = ''
+	length = len(board)
+	for i in range(len(board)):
+		return_string += str(length - i)
+		for tile in board[i]:
+			return_string += '\t'
+			return_string += tile
+		return_string += '\n'
+	
+	return return_string
 
 
 def chat_server():
@@ -18,6 +34,7 @@ def chat_server():
 	# add server socket object to the list of readable connections
 	SOCKET_LIST.append(server_socket)
 	
+	board = [['-' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 	print("Chat server started on port " + str(PORT))
 	
 	while 1:
@@ -31,9 +48,14 @@ def chat_server():
 			if sock == server_socket:
 				sockfd, addr = server_socket.accept()
 				SOCKET_LIST.append(sockfd)
-				print("Client (%s,%s) connected" % addr)
-				
-				broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
+				if len(SOCKET_LIST) % 2 == 0:
+					X_LIST.append(sockfd)
+					print("Client (%s,%s) connected as X" % addr)
+					broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room as X\n" % addr)
+				else:
+					O_LIST.append(sockfd)
+					print("Client (%s,%s) connected as O" % addr)
+					broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room as O\n" % addr)
 			
 			# a message from a client, not a new connection
 			else:
@@ -41,13 +63,19 @@ def chat_server():
 				try:
 					# receiving data from the socket.
 					data = sock.recv(RECV_BUFFER).decode('utf-8')
+					
 					if data:
 						# there is something in the socket
 						broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)
+						print('SOCKET_LIST: ', SOCKET_LIST.index(sock))
+						print('X_LIST: ', X_LIST.index(sock))
+						print('O_LIST: ', O_LIST.index(sock))
 					else:
 						# remove the socket that's broken
 						if sock in SOCKET_LIST:
 							SOCKET_LIST.remove(sock)
+							X_LIST.remove(sock)
+							O_LIST.remove(sock)
 						
 						# at this stage, no data means probably the connection has been broken
 						broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
@@ -72,7 +100,18 @@ def broadcast(server_socket, sock, message):
 				socket.close()
 				# broken socket, remove it
 				if socket in SOCKET_LIST:
-					SOCKET_LIST.remove(socket)
+					try:
+						SOCKET_LIST.remove(socket)
+					except:
+						pass
+					try:
+						X_LIST.remove(socket)
+					except:
+						pass
+					try:
+						O_LIST.remove(socket)
+					except:
+						pass
 
 
 if __name__ == "__main__":
